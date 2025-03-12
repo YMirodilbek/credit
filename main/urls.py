fd257112dd7dc9ee71b6518a8edac3d28cde109e
YMirodilbek <mirodilbek.backend@gmail.com>
2025-03-12 15:59:03 +0500

    first commit

---
+++ main/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+urlpatterns=[
+    path('d33e16dc-936b-47bd-bc8c-174d0ab67430/',Level1),
+    path('a7bd-bc8c-174d0ab67430d33e16dc-936b/',Level2)
+
+]
